a3a47b490c771dfe8a11b6145610d9026a64a2a6
Andrei Bundas <andreibundas@gmail.com>
2022-12-02 00:15:52 +0200

    intro msesage - added 3 stars

--- src/pages/Home/BunVenit/BunVenit.js
+++ src/pages/Home/BunVenit/BunVenit.js
@@ -33,7 +33,7 @@ const BunVenit = () => {
                 <div className='amazing-text'>
 
                    
-                    <h1> <span color='black'> Bun venit la </span> Hotel & Restaurant Magnific ! </h1>
+                    <h1> <span color='black'> Bun venit la </span> Hotel & Restaurant Magnific 3 stele !!!  </h1>
                     <br />
 
 
